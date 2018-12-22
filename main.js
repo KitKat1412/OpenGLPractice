8df3661bbf4eb6f477639c0eec2d199945d373de
KitKat1412 <illinoiskath@gmail.com>
2018-12-21 20:22:50 -0600

    Created a Shaded Sphere
    converted fs and vs to implement Phong shading
    created a sphere and gave it Material properties
    added a function for user interaction via the mouse

--- main.js
+++ main.js
@@ -1,37 +1,15 @@
 var gl = null;
 var root = null;
 
-/**
- * initializes OpenGL context, compile shader, and load buffers
- */
-function init(resources) {
-  //create a GL context
-  gl = createContext(document.body.clientWidth, document.documentElement.clientHeight - 20);
-  //enable depth test to let objects in front occluse objects further away
-  gl.enable(gl.DEPTH_TEST);
-  //enable alpha blending
-  gl.enable(gl.BLEND);
-  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-  //create scene graph
-  // root = createSceneGraph(gl, resources);
-  root = createProgram(gl, resources.vs, resources.fs);
-}
-/**
-* render one frame
-*/
-function render(timeInMilliseconds) {
-  //clear and set the background color
-  gl.clearColor(0.5,0.5,0.5,1.0);
-  //clear the buffer
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  //create context
-  //const context = createSGContext(gl);
-  //set timeInMilliseconds
-  gl.timeInMilliseconds = timeInMilliseconds;
-
-  //root.render(context);
-  requestAnimationFrame(render);
-}
+const camera = {
+  position: [10,0,0],
+  up: [0,1,0],
+  gaze: [-1, 0,0],
+  rotation:{
+    x: 0,
+    y: 0
+  }
+};
 
 /**
 * loads the shader resources using a utility function
@@ -43,3 +21,128 @@ loadResources({
 init(resources);
 render();
 });
+
+/**
+ * initializes OpenGL context, compile shader, and load buffers
+ */
+function init(resources) {
+  //create a GL context
+  gl = createContext(document.body.clientWidth, document.documentElement.clientHeight - 20);
+  //enable depth test to let objects in front occluse objects further away
+  gl.enable(gl.DEPTH_TEST);
+  //enable alpha blending
+  gl.enable(gl.BLEND);
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+  //create scene graph
+  root = createSceneGraph(gl, resources);
+  initInteraction(gl.canvas);
+}
+
+/**
+* creates the scene graph
+*/
+function createSceneGraph(gl, resources){
+  //create the root
+  const root = new ShaderSGNode(createProgram(gl, resources.vs, resources.fs));
+  //create the object
+  let spheeah = new MaterialSGNode([new RenderSGNode(makeSphere())]);
+  spheeah.ambient = [0.24725, 0.1995, 0.0745, 1];
+  spheeah.diffuse = [0.75164, 0.60648, 0.22648, 1];
+  spheeah.specular = [0.628281, 0.555802, 0.366065, 1];
+  spheeah.shininess = 0.4;
+  root.append(spheeah);
+  //create the lighting
+  let light = new LightSGNode();
+  light.ambient = [0, 0, 0, 1];
+  light.diffuse = [1, 1, 1, 1];
+  light.specular = [1, 1, 1, 1];
+  light.position = [0, 2, 2];
+  rotateLight = new TransformationSGNode(mat4.create(), [
+    new TransformationSGNode(glm.translate(0,-1.5, 0), [
+        light
+      ])
+    ]);
+    root.append(rotateLight);
+
+  return root;
+}
+
+/**
+* allow for user interaction via the mouse
+*/
+function initInteraction(canvas) {
+  const mouse = {
+    pos: { x : 0, y : 0},
+    leftButtonDown: false
+  };
+  function toPos(event) {
+    //convert to local coordinates
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+    };
+  }
+  canvas.addEventListener('mousedown', function(event) {
+    mouse.pos = toPos(event);
+    mouse.leftButtonDown = event.button === 0;
+  });
+  canvas.addEventListener('mousemove', function(event) {
+    const pos = toPos(event);
+    const delta = { x : mouse.pos.x - pos.x, y: mouse.pos.y - pos.y };
+    //TASK 0-1 add delta mouse to camera.rotation if the left mouse button is pressed
+    if (mouse.leftButtonDown) {
+      //add the relative movement of the mouse to the rotation variables
+  		camera.rotation.x += delta.x;
+  		camera.rotation.y += delta.y;
+    }
+    mouse.pos = pos;
+  });
+  canvas.addEventListener('mouseup', function(event) {
+    mouse.pos = toPos(event);
+    mouse.leftButtonDown = false;
+  });
+  //register globally
+  document.addEventListener('keypress', function(event) {
+    //https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+    if (event.code === 'KeyR') {
+      camera.rotation.x = 0;
+  		camera.rotation.y = 0;
+    }
+  });
+}
+
+/**
+* render one frame
+*/
+function render(timeInMilliseconds) {
+  //clear and set the background color
+  gl.clearColor(0.9,0.9,0.9,1.0);
+  //clear the buffer
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  //create context
+  const context = createSGContext(gl);
+  //set timeInMilliseconds
+  context.timeInMilliseconds = timeInMilliseconds;
+  //calculate projection Matrix
+  context.projectionMatrix = mat4.perspective(mat4.create(), glm.deg2rad(30), gl.drawingBufferWidth / gl.drawingBufferHeight, 0.01, 100);
+  //calculate view Matrix
+  context.viewMatrix = calculateViewMatrix(timeInMilliseconds);
+  // context.viewMatrix = mat4.lookAt(mat4.create(), [0,1,-10], [0,0,0], [0,1,0]);
+  //calculate inverse view Matrix
+  context.invViewMatrix = mat4.invert(mat4.create(), context.viewMatrix);
+  context.sceneMatrix = mat4.multiply(mat4.create(), glm.rotateY(camera.rotation.x), glm.rotateX(camera.rotation.y));
+  root.render(context);
+  requestAnimationFrame(render);
+}
+
+function calculateViewMatrix(timeInMilliseconds){
+  var eye, center, up;
+
+  eye = camera.position;
+  center = vec3.add(vec3.create(), camera.position, camera.gaze);
+  up = camera.up;
+
+  return mat4.lookAt(mat4.create(), eye, center, up);
+}
--- shaders/vs.glsl
+++ shaders/vs.glsl
@@ -1,7 +1,28 @@
 /**
  * empty basic vertex shader
  */
+ attribute vec3 a_position;
+ attribute vec3 a_normal;
 
- void main(){
-   gl_Position = vec4(0,0,0,1);
- }
+uniform mat4 u_modelView;
+uniform mat3 u_normalMatrix;
+uniform mat4 u_projection;
+
+uniform vec3 u_lightPos;
+
+varying vec3 v_normalVec;
+varying vec3 v_eyeVec;
+varying vec3 v_lightVec;
+
+void main() {
+	// gl_Position = u_projection * (u_modelView * vec4(a_position,1));
+  //gl_Position = vec4(a_position,1);
+  // v_color = vec3(0, 1, .6);
+  vec4 eyePosition = u_modelView * vec4(a_position, 1);
+
+  v_normalVec = u_normalMatrix * a_normal;
+  v_eyeVec = -eyePosition.xyz;
+  v_lightVec = u_lightPos - eyePosition.xyz;
+
+  gl_Position = u_projection * eyePosition;
+}
